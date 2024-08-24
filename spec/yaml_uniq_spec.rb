61c25d3244a401f4bf7a81e6f8dd68f7cf8ec170
Dorian Marié <dorian@dorianmarie.com>
2024-08-24 15:30:10 +0200

    add support for handling command-line arguments and yaml deduplication

--- spec/yaml_uniq_spec.rb
+++ spec/yaml_uniq_spec.rb
@@ -3,4 +3,12 @@
 require "spec_helper"
 
 RSpec.describe "each" do
+  it "works" do
+    expect(`bin/yaml-uniq "- 1" "- 2" "- 1" "- 3"`).to eq(<<~OUTPUT)
+      ---
+      - 1
+      - 2
+      - 3
+    OUTPUT
+  end
 end
